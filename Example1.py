c49351286ad2e5f34e8725d9610a4da1a9f89a26
Indudhar Hiremath <indudar.h@gmail.com>
2017-07-16 18:14:30 +0530

    This is gone to be my first commit to Python Branch A1 folder.

--- Example1.py
+++ Example1.py
@@ -6,5 +6,8 @@ num2 = 6.3
 # Add two numbers
 sum = float(num1) + float(num2)
 
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+if(sum>6):
+    # Display the sum
+    print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+else:
+    print('You are taking a worng path!')
