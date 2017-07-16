764c5d8a0a89a65bf873c786da0417b77967ec78
Indudhar Hiremath <indudar.h@gmail.com>
2017-07-16 16:17:11 +0530

    This is gone to be my first commit to Python master folder.

---
+++ Example51.py
@@ -0,0 +1,13 @@
+
+def strprint(str):
+    for i in str:
+        print(i)
+
+def main():
+    pstr = "i am now inside python"
+    print("Here is a python program")
+    strprint(pstr)
+
+if __name__ == '__main__': main()
+
+    
